eca152bbb7a1bb33bf65ddaf820b6bcd7b53f094
Joao Golias <golias.appsimples@gmail.com>
2018-07-23 22:54:59 -0300

    Finished participant sign up design

--- src/scenes/Participants/Participants.tsx
+++ src/scenes/Participants/Participants.tsx
@@ -1,26 +1,97 @@
+import Button from '@material-ui/core/Button/Button'
+import AddIcon from '@material-ui/icons/Add'
 import * as React from 'react'
 import { CircularProgress } from '../../../node_modules/@material-ui/core'
 import Query from '../../../node_modules/react-apollo/Query'
 import { PARTICIPANTS } from '../../repositories/participants'
 import ParticipantsList from './ParticipantsList'
+import ParticipantsSignUp from './ParticipantsSignUp'
 
-const Participants = () => (
-    <div>
-        <Query
-            query = {PARTICIPANTS}>
-            {({loading, error, data}) => {
-                if (loading) { return <div style={{ height: 400, display: 'flex', alignItems: 'center', justifyContent: 'center'}}><CircularProgress style={{ width: 56, height: 56}}/></div> }
-                if (error) { return `Error! ${error.message}` }
-                return (
-                    <div>
-                        <ParticipantsList
-                           participants = {data.participants.participants}/>
-                    </div>
-                )
-            }}
+class Participants extends React.Component {
+    public state = {
+        openParticipantSignUpDialog: false,
+        openSnackbar: false,
+        resetForm: false,
+        savingParticipant: false,
+    }
 
-        </Query>
-    </div>
-)
+    public handleCloseAddDialog = () => {
+        this.setState({
+            openParticipantSignUpDialog: false
+        })
+    }
+
+    public handleResetForm = () => {
+        this.setState({
+            resetForm: false
+        })
+    }
+
+    public handleAddClick = () => {
+        this.setState({
+            openParticipantSignUpDialog: true
+        })
+    }
+
+    public handleSaveNewParticipant = async(createParticipant: any, newParticipant: any) => {
+        // try {
+        //     this.setState({
+        //         savingActivity: true
+        //     })
+        //     await createParticipant({ variables: { participant: this.mapToInput(newParticipant) } })
+        //     this.setState({
+        //         openParticipantSignUpDialog: false,
+        //         resetForm: true,
+        //         openSnackbar: true,
+        //     })
+        // } catch (err) {
+        //     console.log('err', err)
+        // }
+        // this.setState({
+        //     savingActivity: false
+        // })
+    }
+
+    public mapToInput = (participant) => ({
+        ...participant,
+    })
+
+    public render() {
+        return (
+            <div>
+                <Query
+                    query = {PARTICIPANTS}>
+                    {({loading, error, data}) => {
+                        if (loading) { return <div style={{ height: 400, display: 'flex', alignItems: 'center', justifyContent: 'center'}}><CircularProgress style={{ width: 56, height: 56}}/></div> }
+                        if (error) { return `Error! ${error.message}` }
+                        return (
+                            <div>
+                                <ParticipantsList
+                                   participants = {data.participants.participants}/>
+                                <Button
+                                    variant='fab'
+                                    color='secondary'
+                                    aria-label='add'
+                                    style={{ position: 'absolute', bottom: 64, right: 64 }}
+                                    onClick={this.handleAddClick}
+                                >
+                                    <AddIcon/>
+                                </Button>
+                            </div>
+                        )
+                    }}
+                </Query>
+                <ParticipantsSignUp
+                    open={this.state.openParticipantSignUpDialog}
+                    handleClose={this.handleCloseAddDialog}
+                    handleSave={(newParticipant) => this.handleSaveNewParticipant(undefined, newParticipant)}
+                    // handleResetForm={this.handleResetForm}
+                    // resetForm={this.state.resetForm}
+                    saving={this.state.savingParticipant}                    
+                />
+            </div>
+        )
+    }
+}
 
 export default Participants
---
+++ src/scenes/Participants/ParticipantsSingUpForms.tsx
@@ -0,0 +1,129 @@
+import MenuItem from '@material-ui/core/MenuItem/MenuItem'
+import { Theme, WithStyles, withStyles } from '@material-ui/core/styles'
+import TextField from '@material-ui/core/TextField/TextField'
+import Typography from '@material-ui/core/Typography/Typography'
+import * as classNames from 'classnames'
+import * as React from 'react'
+
+const styles = (theme: Theme) => ({
+    container: {
+        display: 'flex' as any,
+        flexDirection: 'column' as any,
+        flexShrink: 0,
+        margin: 32,
+    },
+    menu: {
+        width: 200,
+    },
+    textField: {
+        marginLeft: theme.spacing.unit,
+        marginRight: theme.spacing.unit,
+    },
+    short: {
+        width: 280,
+    },
+    expansionPanel: {
+        marginTop: 16,
+        boxShadow: 'none',
+        '&:before': {
+            content: 'none',
+        },
+    },
+    expansionPanelSummary: {
+        padding: 0,
+        width: 160,
+    },
+    expansionPanelDetails: {
+        display: 'flex' as any,
+        flexDirection: 'column' as any,
+    },
+    moreOptions: {
+        fontSize: 16,
+        fontWeight: '600' as any,
+    },
+})
+
+
+const mapKind = {
+    'GRADUATING-FOUSP': 'FOUSP - Graduando',
+    'POS-GRADUATING-FOUSP': 'FOUSP - Pós-Graduando',
+    'EXTERNAL-PARTICIPANT': 'Participante Externo',
+}
+
+const kinds = Object.keys(mapKind).map((key) => ({label: mapKind[key], value: key}))
+
+interface IProps {
+    showError: boolean
+    handleChange: (name: string) => (event: any) => void
+    participantModel: any
+    validState: boolean
+    disabled?: boolean
+    showHint?: boolean
+}
+
+class ParticipantSignUpForms extends React.Component<IProps & WithStyles<'container' | 'textField' | 'menu' | 'short' | 'expansionPanel' | 'expansionPanelSummary' | 'moreOptions' | 'expansionPanelDetails'>> {
+    public formatDate = (type: 'start' | 'end') => {
+        return type === 'start' ?
+            (date:Date) => `Início*: ${date && date.toLocaleString('pt-BR').replace(' ', ' - ')}` :
+            (date:Date) => `Término*: ${date && date.toLocaleString('pt-BR').replace(' ', ' - ')}`
+    }
+
+    public render() {
+        const { classes, showError, handleChange, participantModel, validState, disabled, showHint = true } = this.props
+        return (
+            <form className={classes.container} noValidate={true} autoComplete='off'>
+                <Typography className={classes.moreOptions}>Informações básicas</Typography>
+                <TextField
+                    style={{ marginTop: 15 }}
+                    id='name'
+                    disabled={disabled}
+                    label='Nome'
+                    error={showError && !validState }
+                    required={true}
+                    className={classes.textField}
+                    value={participantModel.name}
+                    onChange={handleChange('name')}
+                    margin='normal'
+                />
+
+                <TextField
+                    style={{ marginTop: 15 }}
+                    id='email'
+                    disabled={disabled}
+                    label='Email'
+                    error={showError && !validState }
+                    required={true}
+                    className={classes.textField}
+                    value={participantModel.email}
+                    onChange={handleChange('email')}
+                    margin='normal'
+                />
+                <TextField
+                    style={{ marginTop: 15 }}
+                    id='kind'
+                    disabled={disabled}
+                    select={true}
+                    required={true}
+                    className={classNames(classes.textField, classes.short)}
+                    value={participantModel.kind}
+                    onChange={handleChange('kind')}
+                    SelectProps={{
+                        MenuProps: {
+                            className: classes.menu,
+                        },
+                    }}
+                    helperText={showHint && 'Selecione o tipo de participante*'}
+                    margin='normal'
+                >
+                    {kinds.map(option => (
+                        <MenuItem key={option.value} value={option.value}>
+                            {option.label}
+                        </MenuItem>
+                    ))}
+                </TextField>
+            </form>
+        )
+    }
+}
+
+export default withStyles(styles)(ParticipantSignUpForms)
